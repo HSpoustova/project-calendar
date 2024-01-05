0b73e1601b51d210da771611436f729e877b1c02
Hana Spoustová <bernreih@gmail.com>
2024-01-05 22:48:48 +0100

    pridani fungujiciho kalendare v reactu a css

---
+++ src/components/Calendar.jsx
@@ -0,0 +1,66 @@
+import '../pages/HomePage/style.css';
+
+export const GetMonth = ({ month, year }) => {
+  const months = [
+    'Leden',
+    'Únor',
+    'Březen',
+    'Duben',
+    'Květen',
+    'Červen',
+    'Červenec',
+    'Srpen',
+    'Září',
+    'Říjen',
+    'Listopad',
+    'Prosinec',
+  ];
+
+  return (
+    <p className="current-date">
+      {months[month]} {year}
+    </p>
+  );
+};
+
+export const GetDays = ({ month, year }) => {
+  const lastDateofMonth = new Date(year, month + 1, 0).getDate();
+
+  const firstDayofMonth =
+    new Date(year, month, 1).getDay() === 0
+      ? 7
+      : new Date(year, month, 1).getDay();
+  const lastDateofLastMonth = new Date(year, month, 0).getDate();
+  const lastDayofMonth =
+    new Date(year, month, lastDateofMonth).getDay() === 0
+      ? 7
+      : new Date(year, month, lastDateofMonth).getDay();
+
+  let days = [];
+
+  for (let i = firstDayofMonth; i > 1; i--) {
+    days.push({ class: 'inactive', day: lastDateofLastMonth - i + 2 });
+  }
+
+  for (let i = 1; i <= lastDateofMonth; i++) {
+    let isToday =
+      i === new Date().getDate() &&
+      month === new Date().getMonth() &&
+      year === new Date().getFullYear()
+        ? 'active'
+        : '';
+    days.push({ class: isToday, day: i });
+  }
+
+  for (let i = lastDayofMonth; i < 7; i++) {
+    days.push({ class: 'inactive', day: i - lastDayofMonth + 1 });
+  }
+
+  return days.map((day, index) => {
+    return (
+      <li className={day.class} key={index}>
+        {day.day}
+      </li>
+    );
+  });
+};
---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,61 @@
+import './style.css';
+import { GetMonth } from '../../components/Calendar.jsx';
+import { GetDays } from '../../components/Calendar.jsx';
+import { useState } from 'react';
+
+export const HomePage = () => {
+  const today = new Date();
+  const [currentMonth, setCurrentMonth] = useState(today.getMonth());
+  const [currentYear, setCurrentYear] = useState(today.getFullYear());
+
+  const handleClickNext = () => {
+    if (currentMonth === 11) {
+      setCurrentMonth(0);
+      setCurrentYear(currentYear + 1);
+    } else {
+      setCurrentMonth(currentMonth + 1);
+    }
+  };
+  const handleClickPrev = () => {
+    if (currentMonth === 0) {
+      setCurrentMonth(11);
+      setCurrentYear(currentYear - 1);
+    } else {
+      setCurrentMonth(currentMonth - 1);
+    }
+  };
+
+  console.log(currentMonth, currentYear);
+
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <header>
+          <GetMonth month={currentMonth} year={currentYear} />
+          <div className="icons">
+            <span className="icons__symbols-rounded" onClick={handleClickPrev}>
+              &lt;
+            </span>
+            <span className="icons__symbols-rounded" onClick={handleClickNext}>
+              &gt;
+            </span>
+          </div>
+        </header>
+        <div className="calendar">
+          <ul className="calendar__weeks">
+            <li>Po</li>
+            <li>Út</li>
+            <li>St</li>
+            <li>Čt</li>
+            <li>Pá</li>
+            <li>So</li>
+            <li>Ne</li>
+          </ul>
+          <ul className="calendar__days">
+            <GetDays month={currentMonth} year={currentYear} />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
